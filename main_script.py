82a696006d23fe2e078137cd8bf338a3fd0f8921
Fran Tamarit <fcodpaula.tamarit@gmail.com>
2020-01-11 10:16:41 +0100

    requirements fixed

--- main_script.py
+++ main_script.py
@@ -3,8 +3,12 @@ from package1.wrangle import *
 from package1.enrich import *
 from package1.reporting import *
 
-if __name__ == "__main__":
+
+def main():
     df_master = acquire()
     df_cleaned = wrangling(df_master)
     df_analysis = enrich(df_cleaned)
     reporting(df_analysis)
+
+if __name__ == "__main__":
+    main()
